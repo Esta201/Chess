5e399fb5500057710f00925cc06bccf767c9af1c
Earlene Starling Babette <esta201@aucklanduni.ac.nz>
2021-03-04 12:29:45 +1300

    Created Project

--- Chess/main.cpp
+++ Chess/main.cpp
@@ -4,11 +4,30 @@
 //
 //  Created by Earlene on 4/03/21.
 //
-
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+int main() {
     // insert code here...
+    
+    sf::RenderWindow window( sf::VideoMode( 800, 800), "Chess");
+    sf::Texture chessBoard;
+    chessBoard.loadFromFile("/Assets/chessBoard.png");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent (event)) {
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        window.clear();
+    }
+    
     std::cout << "Hello, World!\n";
     return 0;
 }
